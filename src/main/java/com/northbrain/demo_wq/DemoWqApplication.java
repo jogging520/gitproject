f431d836b447de8d6624c39e43fcca7e23a76952
wq <446734796@qq.com>
2018-07-05 21:14:49 +0800

    init project

---
+++ src/main/java/com/northbrain/demo_wq/DemoWqApplication.java
@@ -0,0 +1,12 @@
+package com.northbrain.demo_wq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWqApplication.class, args);
+    }
+}
